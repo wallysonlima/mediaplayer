efec7d18ae6c0049cf9e0eaeaa404f1d5fc291e8
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-11-03 12:07:47 -0300

    Change the project, build a new interface

--- app/src/main/java/wallyson/lima/mediaplayerapp/MainActivity.java
+++ app/src/main/java/wallyson/lima/mediaplayerapp/MainActivity.java
@@ -9,86 +9,10 @@ import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private MediaPlayer mediaPlayer;
-    private Button playButton;
-    private SeekBar mSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mSeekBar = (SeekBar) findViewById(R.id.mSeekBar);
-
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.justin_timberlake);
-        mSeekBar.setMax(mediaPlayer.getDuration());
-
-        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if(fromUser) {
-                    mediaPlayer.seekTo(progress);
-                }
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                  int duration = mp.getDuration();
-                  String mDuration = String.valueOf(duration/60000);
-
-                Toast.makeText(getApplicationContext(), "duration " + mDuration, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        playButton = (Button) findViewById(R.id.playButton);
-        playButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mediaPlayer.isPlaying()) {
-                    // stop and give option to start again
-                    pauseMusic();
-                } else {
-                    startMusic();
-                }
-            }
-        });
-    }
-
-    public void pauseMusic() {
-        if( mediaPlayer != null ) {
-            mediaPlayer.pause();
-            playButton.setText("Play");
-        }
-    }
-
-    public void startMusic() {
-        if (mediaPlayer != null ) {
-            mediaPlayer.start();
-            playButton.setText("Pause");
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
-            mediaPlayer.stop();
-            mediaPlayer.release();
-            mediaPlayer = null;
-        }
-
-        super.onDestroy();
     }
 }
